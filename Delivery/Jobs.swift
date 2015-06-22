c96e15b7259df7c265ede1f3e63f3ceccfc44242
shannonshsu <shannonshsu@gmail.com>
2015-06-22 15:10:51 -0400

    Drafted Job and JobManager classes.

--- Delivery/Jobs.swift
+++ Delivery/Jobs.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 
-struct job {
+var jobMgr = JobManager();
+
+class Job {
+    var ID = 10;
     var seller_address = "Seller Address";
     var seller_available_time = "All the time";
     var seller_name = "Foo";
@@ -20,12 +23,38 @@ struct job {
     var buyer_phone = "222-222-2222";
     
     var expiration_time = "10:10:10 20/06/2015";
-    var bounty = 10;
-    var amount_due = 100;
+    var bounty = 10.0;
+    var amount_due = 10.0;
     var item_name = "item_name";
     var item_quantity = 1;
-}
     
-class Jobs: NSObject {
-    var jobs = [job]();
+    var claimed = false;
+    
+    init (identifier: Int) {
+        self.ID = identifier;
+    }
+    
 }
+
+class JobManager: NSObject {
+    var unclaimed_jobs = [Job]();
+    
+ /*   override init() {
+        super.init();
+        var job1 = Job(identifier: 1);
+        self.addJob(job1);
+        print(self.unclaimed_jobs[0].buyer_name);
+    }*/ // Test Code
+    
+    func addJob(targetJob: Job){
+        unclaimed_jobs.append(targetJob);
+    }
+    
+    func claimJob(targetJob: Job, index: Int){
+        targetJob.claimed = true;
+        unclaimed_jobs.removeAtIndex(index);
+    }
+    
+}
+
+
