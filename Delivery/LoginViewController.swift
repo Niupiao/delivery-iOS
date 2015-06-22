ff1d78ec9429466f66de1d71edae3e28a964dbc9
Mohamed Soussi <modsoussi@Mohameds-MacBook-Pro.local>
2015-06-22 15:27:54 -0400

    login scree, used cowhead as logo

---
+++ Delivery/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Delivery
+//
+//  Created by Mohamed Soussi on 6/22/15.
+//  Copyright (c) 2015 NiuPiao. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var keyField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func loginButtonPressed(sender: UIButton) {
+    }
+}
