224dbafb101855619c02dd0c88e27c62a4146909
Mohamed Soussi <modsoussi@Mohameds-MacBook-Pro.local>
2015-06-23 00:21:09 -0400

    added login view, can't yer login

---
+++ Delivery/LoginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.swift
+//  Delivery
+//
+//  Created by Mohamed Soussi on 6/22/15.
+//  Copyright (c) 2015 NiuPiao. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var keyField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func textFieldDoneEditing(sender: UITextField) {
+        sender.resignFirstResponder()
+    }
+    
+    @IBAction func backgroundTap(sender: UIControl){
+        keyField.resignFirstResponder()
+    }
+    
+    //MARK: Navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // do login stuff
+    }
+}
